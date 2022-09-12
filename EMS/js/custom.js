df053942cf9dd4bbdca83dca045e458987d3b2b4
DizitalPods <108466643+DizitalPods@users.noreply.github.com>
2022-09-12 10:47:27 +0530

    New Update uploaded

--- EMS/js/custom.js
+++ EMS/js/custom.js
@@ -15,6 +15,25 @@
     //     Reset();
     // }
 
+    // Left side bar Active action Start
+
+
+    // var header = document.getElementById("accordionSidebar");
+    // var btns = header.getElementsByClassName("nav-item");
+    // for (var i = 0; i < nav-item.length; i++) {
+    //     nav-item[i].addEventListener("click", function() {
+    //         var current = document.getElementsByClassName("active");
+    //         current[0].className = current[0].className.replace(" active", "");
+    //         this.className += " active";
+    //     });
+    // }
+
+
+    // $('.nav-item').on('click', function() {
+    //     $(this).addClass('active').siblings('li').removeClass('active');
+    //   });
+
+    // Left side bar Active action  End
 
 
     // Toggle the side navigation
@@ -83,7 +102,8 @@
     }
     render()
 
-    document.getElementById("addsection").onclick = function() {
+    if (document.getElementById("addsection")) {
+        document.getElementById("addsection").onclick = function() {
             // var container = document.getElementById("add-date");
             // var section = document.getElementById("datepicker");
             // container.appendChild(section.cloneNode(true));
@@ -92,32 +112,91 @@
 
             render()
         }
-        // $('.datepicker').datepicker({
-        //     labelMonthNext: 'Go to the next month',
-        //     labelMonthPrev: 'Go to the previous month',
-        //     labelMonthSelect: 'Pick a month from the dropdown',
-        //     labelYearSelect: 'Pick a year from the dropdown',
-        //     selectMonths: true,
-        //     selectYears: true
-        // })
+
+    }
+    // $('.datepicker').datepicker({
+    //     labelMonthNext: 'Go to the next month',
+    //     labelMonthPrev: 'Go to the previous month',
+    //     labelMonthSelect: 'Pick a month from the dropdown',
+    //     labelYearSelect: 'Pick a year from the dropdown',
+    //     selectMonths: true,
+    //     selectYears: true
+    // })
 
     const input = document.querySelector("input[type=file]")
     console.log(input)
-    input.onchange = function(e) {
+    if (input) {
+        input.onchange = function(e) {
 
-        var file = e.target.files[0]
-        var filename = file.name
-        var extension = file.type
+            var file = e.target.files[0]
+            var filename = file.name
+            var extension = file.type
 
-        var blob = new Blob([file], { type: extension })
+            var blob = new Blob([file], { type: extension })
 
-        console.log(blob)
+            console.log(blob)
 
-        var a = document.createElement("a")
-        a.href = URL.createObjectURL(blob)
-        a.download = filename
-        a.click();
+            var a = document.createElement("a")
+            a.href = URL.createObjectURL(blob)
+            a.download = filename
+            a.click();
 
 
+
+        }
     }
+
+    $(document).ready(function($) {
+        $(".clickable-row").click(function() {
+            window.location = $(this).data("href");
+        });
+    });
+
+    // Candidate profile page edit button functionality start
+
+    const editBtn = document.getElementById("candidate_profile_edit")
+    const checkBtn = document.getElementById("candidate_profile_check")
+    checkBtn.style.display = "none"
+    const tableDropdownEls = document.querySelectorAll('#hr-interview-status');
+    const tableTexts = document.querySelectorAll("#hr-interview-status-text");
+    const hrInterviewBlock = document.getElementById("hr-interview-block");
+    const backdrop = document.getElementById("backdrop");
+
+    tableDropdownEls.forEach((el, index) => {
+        tableTexts[index].innerHTML = el.value
+    })
+
+
+    editBtn.addEventListener('click', function() {
+        backdrop.style.zIndex = "1"
+        tableDropdownEls.forEach((el, index) => {
+            el.style.display = "block";
+            tableTexts[index].innerHTML = el.value;
+        })
+        tableTexts.forEach(el => {
+            el.style.display = "none";
+        })
+        checkBtn.style.display = "block"
+        editBtn.style.display = "none"
+        hrInterviewBlock.style.zIndex = "1"
+    })
+
+    checkBtn.addEventListener('click', function() {
+        backdrop.style.zIndex = "0"
+        tableDropdownEls.forEach((el, index) => {
+            el.style.display = "none";
+            tableTexts[index].innerHTML = el.value
+        })
+        tableTexts.forEach(el => {
+            el.style.display = "block";
+        })
+        editBtn.style.display = "block"
+        checkBtn.style.display = "none"
+        hrInterviewBlock.style.zIndex = "0"
+    })
+
+    // candidate profile page edit button functionality end 
+
+
+
 })(jQuery); // End of use strict
